c1b70b2aba820cd31d38d4a3a2bb8864dd19da23
Blaz Solar <solarblaz@gmail.com>
2017-08-08 23:38:44 +0200

    Platform tools

--- config/circleci/run-tests.sh
+++ config/circleci/run-tests.sh
@@ -24,6 +24,7 @@ cd vlc
 git checkout bdb4031
 cd ../..
 $ANDROID_SDK/tools/bin/sdkmanager "platforms;android-26"
+$ANDROID_SDK/tools/bin/sdkmanager "platform-tools"
 ./gradlew vlcBuild
 
 # if [ $CIRCLE_NODE_INDEX -eq 0 ]
