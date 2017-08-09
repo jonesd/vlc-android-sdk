bbe165720cf81407a0bca1fcc6e6a8df11eb2783
Blaz Solar <solarblaz@gmail.com>
2017-08-09 16:38:03 +0200

    Dependencies

--- config/circleci/run-tests.sh
+++ config/circleci/run-tests.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get -y upgrade
 apt-get -y install automake ant autopoint cmake build-essential libtool \
      patch pkg-config protobuf-compiler ragel subversion unzip git \
-     openjdk-8-jre openjdk-8-jdk wget python
+     openjdk-8-jre openjdk-8-jdk wget python gettext
 apt-get -y install autoconf gawk gcc g++ m4
 
 mkdir $HOME/android-sdk
