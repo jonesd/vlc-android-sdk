40a44e35684fdc856bca7f1fb393fb3960705539
Blaz Solar <solarblaz@gmail.com>
2017-08-13 17:00:47 +0200

    Wget and python

--- config/circleci/install-packages.sh
+++ config/circleci/install-packages.sh
@@ -5,8 +5,8 @@ sudo apt-get update
 
 sudo apt-get install automake ant autopoint cmake build-essential libtool \
      patch pkg-config protobuf-compiler ragel subversion unzip git \
-    openjdk-8-jre openjdk-8-jdk
-    
+    openjdk-8-jre openjdk-8-jdk wget python
+
 sudo apt-get -y install zlib1g:i386 libstdc++6:i386 libc6:i386
 
 # sudo apt-get update
