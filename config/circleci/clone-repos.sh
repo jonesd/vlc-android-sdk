4ad686044e9a1833e3a71ea7765ffedbd19da669
Blaz Solar <solarblaz@gmail.com>
2017-08-13 16:33:39 +0200

    Clone through compile operation

--- config/circleci/clone-repos.sh
+++ config/circleci/clone-repos.sh
@@ -3,7 +3,8 @@
 git clone https://code.videolan.org/videolan/vlc-android.git vlc-android
 cd vlc-android
 git checkout 2.1.16
-git clone http://git.videolan.org/git/vlc.git vlc
-cd vlc
-git checkout bdb4031
-cd ../..
+cd ..
+# git clone http://git.videolan.org/git/vlc.git vlc
+# cd vlc
+# git checkout bdb4031
+# cd ../..
