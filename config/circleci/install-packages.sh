d481ce647c8caf38ecba232530c903347f0fd911
Blaz Solar <solarblaz@gmail.com>
2017-08-13 14:22:45 +0200

    Sudo

--- config/circleci/install-packages.sh
+++ config/circleci/install-packages.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-apt-get update
-apt-get -y upgrade
-apt-get -y install automake ant autopoint cmake build-essential libtool \
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install automake ant autopoint cmake build-essential libtool \
      patch pkg-config protobuf-compiler ragel subversion unzip git \
      openjdk-8-jre openjdk-8-jdk wget python gettext cvs yasm
-apt-get -y install autoconf gawk gcc g++ m4
+sudo apt-get -y install autoconf gawk gcc g++ m4
 
 dpkg --add-architecture i386
-apt-get update
-apt-get -y install zlib1g:i386 libstdc++6:i386 libc6:i386
+sudo apt-get update
+sudo apt-get -y install zlib1g:i386 libstdc++6:i386 libc6:i386
