97e692fae31fad6bf9643628032482fe114a85d9
Blaz Solar <solarblaz@gmail.com>
2017-08-13 17:23:05 +0200

    Debugging

--- config/circleci/install-packages.sh
+++ config/circleci/install-packages.sh
@@ -9,6 +9,8 @@ sudo apt-get -y install automake ant autopoint cmake build-essential libtool \
 
 sudo apt-get -y install zlib1g:i386 libstdc++6:i386 libc6:i386
 
+sudo ldconfig -v
+
 # sudo apt-get update
 # sudo apt-get -y upgrade
 # sudo apt-get -y install automake ant autopoint cmake build-essential libtool \
