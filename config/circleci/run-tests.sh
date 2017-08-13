21c906f5e366f19aef7a0a5ba1a644dedd02ce3b
Blaz Solar <solarblaz@gmail.com>
2017-08-13 12:22:31 +0200

    Latest version

--- config/circleci/run-tests.sh
+++ config/circleci/run-tests.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-git clone https://code.videolan.org/videolan/vlc-android.git vlc-android
-cd vlc-android
-git checkout 2.1.15
-git clone http://git.videolan.org/git/vlc.git vlc
-cd vlc
-git checkout bdb4031
-cd ../..
 ./gradlew vlcBuildX86
