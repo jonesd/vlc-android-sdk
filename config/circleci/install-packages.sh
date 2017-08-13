73bda1e0fbdc0ed4e51d94cd4b3e30b2f64174ec
Blaz Solar <solarblaz@gmail.com>
2017-08-13 11:59:15 +0200

    Env vars

--- config/circleci/install-packages.sh
+++ config/circleci/install-packages.sh
@@ -8,5 +8,5 @@ apt-get -y install automake ant autopoint cmake build-essential libtool \
 apt-get -y install autoconf gawk gcc g++ m4
 
 dpkg --add-architecture i386
-apt-get -y update
+apt-get update
 apt-get -y install zlib1g:i386 libstdc++6:i386 libc6:i386
