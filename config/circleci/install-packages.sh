dbdc170633da581dc988ca9da730612e0c273a92
Blaz Solar <solarblaz@gmail.com>
2017-08-14 13:34:59 +0200

    more 32

--- config/circleci/install-packages.sh
+++ config/circleci/install-packages.sh
@@ -13,7 +13,7 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install automake ant autopoint cmake build-essential libtool \
      patch pkg-config protobuf-compiler ragel subversion unzip git \
-     openjdk-8-jre openjdk-8-jdk wget python gettext cvs yasm libtool-bin zlib1g:i386 libstdc++6:i386 libc6:i386
+     openjdk-8-jre openjdk-8-jdk wget python gettext cvs yasm libtool-bin zlib1g:i386 libstdc++6:i386 libc6:i386 libc6-dev:i386
 sudo apt-get -y install autoconf gawk gcc g++ m4
 
 # sudo dpkg --add-architecture i386
