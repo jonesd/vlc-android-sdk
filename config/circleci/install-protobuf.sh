1f01b979fc7ddb1569bbb92cef627678b9b41c99
Blaz Solar <solarblaz@gmail.com>
2017-09-10 20:19:29 +0200

    Changes

--- config/circleci/install-protobuf.sh
+++ config/circleci/install-protobuf.sh
@@ -17,15 +17,15 @@
 #sudo chwon [user] /usr/local/bin/protoc
 #sudo chwon -R [user] /usr/local/include/google
 
-#! /bin/bash
-wget https://github.com/google/protobuf/releases/download/v2.6.1/protobuf-2.6.1.tar.gz
-tar xzf protobuf-2.6.1.tar.gz
-cd protobuf-2.6.1
-sudo apt-get update
-sudo apt-get install build-essential
-sudo ./configure
-sudo make
-sudo make check
-sudo make install 
-sudo ldconfig
-protoc --version
+##! /bin/bash
+#wget https://github.com/google/protobuf/releases/download/v2.6.1/protobuf-2.6.1.tar.gz
+#tar xzf protobuf-2.6.1.tar.gz
+#cd protobuf-2.6.1
+#sudo apt-get update
+#sudo apt-get install build-essential
+#sudo ./configure
+#sudo make
+#sudo make check
+#sudo make install 
+#sudo ldconfig
+#protoc --version
