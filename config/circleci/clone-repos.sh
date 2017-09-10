836ba0017e465d53945a766b99fa2b54f5b9c0b3
Blaz Solar <solarblaz@gmail.com>
2017-09-10 16:37:25 +0200

    Bumping version

--- config/circleci/clone-repos.sh
+++ config/circleci/clone-repos.sh
@@ -2,7 +2,7 @@
 
 git clone https://code.videolan.org/videolan/vlc-android.git vlc-android
 cd vlc-android
-git checkout 2.1.16
+git checkout 2.1.19
 cd ..
 # git clone http://git.videolan.org/git/vlc.git vlc
 # cd vlc
