d1d40127a06ed6383029c16be6ce5bcfe1477b5f
Blaz Solar <solarblaz@gmail.com>
2017-09-10 18:06:48 +0200

    protoc version
    Signed-off-by: Blaz Solar <solarblaz@gmail.com>

--- config/circleci/install-protobuf.sh
+++ config/circleci/install-protobuf.sh
@@ -1,10 +1,10 @@
 
 # Make sure you grab the latest version
-curl -OL https://github.com/google/protobuf/releases/download/v3.2.0/protoc-3.2.0-linux-x86_64.zip
+curl -OL https://github.com/google/protobuf/releases/download/v3.1.0/protoc-3.1.0-linux-x86_64.zip
 
 # Unzip
-unzip protoc-3.2.0-linux-x86_64.zip -d protoc3
-ls
+unzip protoc-3.1.0-linux-x86_64.zip -d protoc3
+
 # Move protoc to /usr/local/bin/
 sudo mv protoc3/bin/* /usr/local/bin/
 
