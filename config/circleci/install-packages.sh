b27548e3bf1f216f64fa51453106db7136b9b3e3
Blaz Solar <solarblaz@gmail.com>
2017-09-10 16:55:15 +0200

    libiconv

--- config/circleci/install-packages.sh
+++ config/circleci/install-packages.sh
@@ -42,3 +42,5 @@ sudo apt-get -y install autoconf gawk gcc g++ m4
 # sudo dpkg --add-architecture i386
 # sudo apt-get update
 # sudo apt-get -y install zlib1g:i386 libstdc++6:i386 libc6:i386
+
+sudo apt-get -y install libiconv* liburiparser*"
