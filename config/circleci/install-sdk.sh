0b736af8cb828f8cd4f8133a7c9d6d8d54b27de3
Blaz Solar <solarblaz@gmail.com>
2017-10-24 22:20:55 +0200

    License update

--- config/circleci/install-sdk.sh
+++ config/circleci/install-sdk.sh
@@ -11,6 +11,7 @@ unzip -o android-ndk-r14b-linux-x86_64.zip -d $ANDROID_HOME/
 mv $ANDROID_NDK/android-ndk-r14b/* $ANDROID_NDK/
 mkdir "$ANDROID_SDK/licenses"
 echo "8933bad161af4178b1185d1a37fbf41ea5269c55" > "$ANDROID_SDK/licenses/android-sdk-license"
+echo "d56f5187479451eabf01fb78af6dfcb131a6481e" > "$ANDROID_SDK/licenses/android-sdk-license"
 echo "84831b9409646a918e30573bab4c9c91346d8abd" > "$ANDROID_SDK/licenses/android-sdk-preview-license"
 
 $ANDROID_SDK/tools/bin/sdkmanager "platforms;android-26"
