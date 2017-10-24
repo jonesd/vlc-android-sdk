573971c4857418bb5d1054f882c860e11cb9dae9
Blaz Solar <solarblaz@gmail.com>
2017-10-24 22:01:28 +0200

    Zvib patch

--- config/circleci/clone-repos.sh
+++ config/circleci/clone-repos.sh
@@ -2,10 +2,11 @@
 
 git clone https://code.videolan.org/videolan/vlc-android.git vlc-android
 cd vlc-android
-git checkout 2.5.3
+git checkout 2.5.5
 # cd ..
 git clone http://git.videolan.org/git/vlc.git vlc
 cd vlc
 git checkout 1c02164
 cd ../..
 patch -p1 vlc-android/vlc/extras/tools/bootstrap < protobuff.patch 
+patch -p1 vlc-android/compile-libvlc.sh < zvib.patch 
