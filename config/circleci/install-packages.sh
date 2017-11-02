dce24c2fb57ab5762dd6d1ea08112b8bfd0c868f
Blaz Solar <solarblaz@gmail.com>
2017-11-02 21:56:40 +0100

    Update packages

--- config/circleci/install-packages.sh
+++ config/circleci/install-packages.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-sudo dpkg --add-architecture i386
+# sudo dpkg --add-architecture i386
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get -y install unzip
+# sudo apt-get -y install unzip
 
-sudo apt-get -y install automake ant autopoint cmake build-essential libtool \
-     patch pkg-config ragel subversion unzip git \
-     openjdk-8-jre openjdk-8-jdk wget curl python gettext cvs yasm libtool-bin zlib1g:i386 libstdc++6:i386 libc6:i386 libc6-dev:i386
-sudo apt-get -y install autoconf gawk gcc g++ m4
+# sudo apt-get -y install automake ant autopoint cmake build-essential libtool \
+#      patch pkg-config ragel subversion unzip git \
+#      openjdk-8-jre openjdk-8-jdk wget curl python gettext cvs yasm libtool-bin zlib1g:i386 libstdc++6:i386 libc6:i386 libc6-dev:i386
+# sudo apt-get -y install autoconf gawk gcc g++ m4
 
-sudo apt-get -y install libiconv* liburiparser*
-sudo apt-get -y install protobuf-compiler
+# sudo apt-get -y install libiconv* liburiparser*
+# sudo apt-get -y install protobuf-compiler
