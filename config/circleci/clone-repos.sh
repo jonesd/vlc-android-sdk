c02e30a3271b58027a48e3d073586b5f6433ca9c
Blaz Solar <solarblaz@gmail.com>
2017-11-02 21:09:59 +0100

    Update version

--- config/circleci/clone-repos.sh
+++ config/circleci/clone-repos.sh
@@ -2,10 +2,10 @@
 
 git clone https://code.videolan.org/videolan/vlc-android.git vlc-android
 cd vlc-android
-git checkout 2.5.5
+git checkout 2.5.7
 git clone http://git.videolan.org/git/vlc.git vlc
 cd vlc
-git checkout 1c02164
+git checkout 8a3958c
 cd ../..
-patch -p1 vlc-android/vlc/extras/tools/bootstrap < protobuff.patch 
+# patch -p1 vlc-android/vlc/extras/tools/bootstrap < protobuff.patch
 patch -p1 vlc-android/compile-libvlc.sh < zvbi.patch
