ea06c298d5dead75eea3ed65d8c2bc372a9e195f
Blaz Solar <solarblaz@gmail.com>
2017-11-03 23:57:31 +0100

    Cleanng up code

--- config/circleci/clone-repos.sh
+++ config/circleci/clone-repos.sh
@@ -3,10 +3,11 @@
 git clone https://code.videolan.org/videolan/vlc-android.git vlc-android
 cd vlc-android
 git checkout 2.5.7
-git clone http://git.videolan.org/git/vlc.git vlc
-cd vlc
-git checkout 8a3958c
-cd ../..
+# git clone http://git.videolan.org/git/vlc.git vlc
+# cd vlc
+# git checkout 8a3958c
+#cd ../..
+cd ..
 # patch -p1 vlc-android/vlc/extras/tools/bootstrap < protobuff.patch
 # patch -p1 vlc-android/compile-libvlc.sh < zvbi.patch
 patch -p1 vlc-android/compile-libvlc.sh < compile-libvlc.patch
--- config/circleci/run-tests.sh
+++ config/circleci/run-tests.sh
@@ -2,10 +2,12 @@
 
 cd vlc-android
 
-# Arm v7
+echo "Build armeabi-v7a"
 ./compile.sh -l --no-ml --release -a armeabi-v7a
-mkdir -p ../src/main/jniLibs/armeabi-v7a
-cp /libvlc/jni/libs/armeabi-v7a/ ../src/main/jniLibs/armeabi-v7a/
 
-# Java files
-cp /libvlc/src/ ../src/main/java
+echo "Copy native lib for armeabi-v7a"
+mkdir -p ../src/main/jniLibs/armeabi-v7a
+cp libvlc/jni/libs/armeabi-v7a/ ../src/main/jniLibs/armeabi-v7a/
+
+echo "Copy java files"
+cp libvlc/src/ ../src/main/java
