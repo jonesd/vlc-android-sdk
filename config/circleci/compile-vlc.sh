331bbeea60f78bff92ecf0c48cd2e79d19b5f5cc
Blaz Solar <solarblaz@gmail.com>
2017-11-04 00:24:21 +0100

    Updates

--- config/circleci/compile-vlc.sh
+++ config/circleci/compile-vlc.sh
@@ -7,7 +7,7 @@ echo "Build armeabi-v7a"
 
 echo "Copy native lib for armeabi-v7a"
 mkdir -p ../src/main/jniLibs/armeabi-v7a
-cp libvlc/jni/libs/armeabi-v7a/ ../src/main/jniLibs/armeabi-v7a/
+cp -r libvlc/jni/libs/armeabi-v7a/ ../src/main/jniLibs/armeabi-v7a/
 
 echo "Copy java files"
-cp libvlc/src/ ../src/main/java
+cp -r libvlc/src/ ../src/main/java
