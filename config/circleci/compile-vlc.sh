dd8d85de6766fa11e09a796e3b683a9982eb1d5f
Blaz Solar <solarblaz@gmail.com>
2017-11-04 11:27:21 +0100

    Updates

--- config/circleci/compile-vlc.sh
+++ config/circleci/compile-vlc.sh
@@ -11,9 +11,11 @@ do
 
     echo "Copy native lib for $a"
     mkdir -p ../src/main/jniLibs/$a
-    cp -r libvlc/jni/libs/$a/ ../src/main/jniLibs/$a/
-
+    cp -r libvlc/jni/libs/$a/ ../src/main/jniLibs/
 done
 
 echo "Copy java files"
 cp -r libvlc/src/ ../src/main/java
+cd ..
+
+./gradlew assDeb
