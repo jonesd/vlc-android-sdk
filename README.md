00cb7fd39a33746dd094c0ec574f9bde7c5f746f
Blaž Šolar <solarblaz@gmail.com>
2018-01-27 10:20:31 +0100

    Bumping to version 2.9.0

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ buildscript {
 }
 
 dependencies {
-  compile "com.butterproject:vlc-android-sdk:2.5.14"
+  compile "com.butterproject:vlc-android-sdk:2.9.0"
 }
 ```
 
--- config/circleci/clone-repos.sh
+++ config/circleci/clone-repos.sh
@@ -5,7 +5,7 @@ git clone https://code.videolan.org/videolan/vlc-android.git vlc-android
 cd vlc-android
 
 echo "Checkout tag"
-git checkout 2.5.17
+git checkout 2.9.0
 cd ..
 
 echo "Patch compile options"
